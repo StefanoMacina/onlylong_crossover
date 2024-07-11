478812371f033aa5adab95bc9569d262ad5e9d64
macina <macina@key-4.com>
2024-07-11 17:42:10 +0200

    add log and sizing

--- Strategies.py
+++ Strategies.py
@@ -5,15 +5,30 @@ class SmaCross(bt.Strategy):
     # list of parameters which are configurable for the strategy
     params = dict(
         pfast=10,  # period for the fast moving average
-        pslow=30  # period for the slow moving average
+        pslow=30,  # period for the slow moving average
     )
+
+    def log(self, txt, dt=None):
+        dt = dt or self.datas[0].datetime.date(0)
+        print(f'{dt.isoformat()}, {txt}')
+
+    def notify_order(self, order):
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                self.log(f'BUY EXECUTED, {order.executed.size} shares at {order.executed.price}')
+            elif order.issell():
+                self.log(f'SELL EXECUTED, {order.executed.size} shares at {order.executed.price}')
+        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
+            self.log('Order Canceled/Margin/Rejected')
 
     def __init__(self):
         sma1 = bt.ind.SMA(period=self.p.pfast)  # fast moving average
         sma2 = bt.ind.SMA(period=self.p.pslow)  # slow moving average
         self.crossover = bt.ind.CrossOver(sma1, sma2)  # crossover signal
 
+
     def next(self):
+
         if not self.position:  # not in the market
             if self.crossover > 0:  # if fast crosses slow to the upside
                 self.buy()  # enter long
@@ -47,3 +62,16 @@ class Whoa(bt.Strategy):
                 # BUY, BUY, BUY!!! (with all possible default parameters)
                 self.log('BUY CREATE, %.2f' % self.dataclose[0])
                 self.buy()
+
+
+class PrintData(bt.Strategy):
+    def __init__(self):
+        self.data_close = self.datas[0].close
+        self.data_open = self.datas[0].open
+        self.data_high = self.datas[0].high
+        self.data_low = self.datas[0].low
+        self.data_volume = self.datas[0].volume
+
+    def next(self):
+
+       print( self.datas[0].datetime.date(0) ,self.data_close[0])
--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
+from datetime import datetime
 import yfinance as yf
 import backtrader as bt
-
+import pandas as pd
 import Strategies
+import mplfinance as mpf
+
 
 
 if __name__ == '__main__':
+
     cerebro = bt.Cerebro()
     df = yf.download('GOOGL', start='2010-07-01')
-    data = bt.feeds.PandasData(dataname=df)
-    cerebro.adddata(data)
-    cerebro.addstrategy(Strategies.Whoa)
-    print('Starting portfolio value %.2f' % cerebro.broker.get_value())
+    cerebro.adddata(bt.feeds.PandasData(dataname=df))
+    cerebro.addstrategy(Strategies.SmaCross)
+    cerebro.addsizer(bt.sizers.PercentSizer, percents=2)
+    print('portfolio starting value %.2f' % cerebro.broker.getvalue())
     cerebro.run()
-    print('final portfolio value %.2f' % cerebro.broker.get_value())
+    print('portfolio final value %.2f' % cerebro.broker.getvalue())
     cerebro.plot()
+
+    #print(df)
+
